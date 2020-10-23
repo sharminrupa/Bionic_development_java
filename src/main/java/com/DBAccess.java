54687f6d0ac860f660e986254abcf5ca0fe0eb95
Sharmin Akter <54450793+sharminrupa@users.noreply.github.com>
2020-10-23 18:04:08 +0600

    creat a dao

---
+++ src/main/java/com/DBAccess.java
@@ -0,0 +1,17 @@
+package com;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBAccess {
+    private static Connection conn = null;
+    
+    public static Connection getConn() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        if (conn == null) {
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/bionicfrom", "root", "");
+        }
+        return conn;        
+    }
+}
---
+++ src/main/java/com/bionicBean.java
@@ -0,0 +1,56 @@
+
+package com;
+
+public class bionicBean {
+    private String id;
+    private String name;
+    private String Mobile;
+    private String email;
+    private String message;  
+
+    public bionicBean() {
+    }
+    
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(String Mobile) {
+        this.Mobile = Mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    
+}
